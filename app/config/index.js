d7e8f504000fb7503a8b21886c85266336759503
Jackson Yuan <jacksonyuanjx@hotmail.com>
2020-03-28 12:38:33 -0700

    set up some stuff w/ pusher

--- app/config/index.js
+++ app/config/index.js
@@ -1,13 +1,7 @@
 require('dotenv').config();
 
 const config = {
-    pusher: {
-        appId: "971587",
-        key: "8a8448593705331f8283",
-        secret: "f362ffdac8b6be9b6c29",
-        cluster: "us3",
-        encrypted: true
-    }
+
 };
 
 
