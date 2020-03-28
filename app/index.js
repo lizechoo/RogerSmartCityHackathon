cf2b93f40351cc49c2f5294eccc5e11a2e4cf2ab
Jackson Yuan <jacksonyuanjx@hotmail.com>
2020-03-28 12:12:18 -0700

    basic html dashboard framework

---
+++ app/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// const router = express.Router();
+app.use(express.static('public'));
+
+// app.get("/", (req, res) => {
+//     res.send("TEST NEW MESSAGE!");
+// });
+
+app.listen(3000, () => console.log("Server running on port 3000"));
+
+// router.get("/get_stuff", (req, res) => {
+//     console.log("/get_stuff endpoint called");
+//     res.send({
+//         message: "endpoint called"
+//     });
+// });
