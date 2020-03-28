241ce133aacfce407a2c3dd874eeda41a9813de4
Jackson Yuan <jacksonyuanjx@hotmail.com>
2020-03-28 12:38:04 -0700

    .

---
+++ app/config/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const config = {
+    pusher: {
+        appId: "971587",
+        key: "8a8448593705331f8283",
+        secret: "f362ffdac8b6be9b6c29",
+        cluster: "us3",
+        encrypted: true
+    }
+};
+
+
+module.exports = config;
--- app/index.js
+++ app/index.js
@@ -2,13 +2,27 @@ const express = require("express");
 const app = express();
 
 // const router = express.Router();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // app.get("/", (req, res) => {
 //     res.send("TEST NEW MESSAGE!");
 // });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
+
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+    appId: "971587",
+    key: "8a8448593705331f8283",
+    secret: "f362ffdac8b6be9b6c29",
+    cluster: "us3",
+    encrypted: true
+});
+
+pusher.trigger("my-channel", "my-event", {
+    message: "hello world"
+});
 
 // router.get("/get_stuff", (req, res) => {
 //     console.log("/get_stuff endpoint called");
