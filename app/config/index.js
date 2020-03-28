63c1ff8a9ff67ff32d5b16b56c823c8cc3c62921
Jackson Yuan <jacksonyuanjx@hotmail.com>
2020-03-28 12:55:06 -0700

    set up basic pusher config

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 
 const config = {
-
+    pusher: {
+        appId: process.env.PUSHER_APP_ID,
+        key: process.env.PUSHER_KEY,
+        secret: process.env.PUSHER_SECRET,
+        cluster: process.env.PUSHER_CLUSTER,
+        useTLS: true,
+    }
 };
 
 
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const config = require("./config/index");
 
 // const router = express.Router();
 app.use(express.static("public"));
@@ -13,11 +14,11 @@ app.listen(3000, () => console.log("Server running on port 3000"));
 const Pusher = require("pusher");
 
 const pusher = new Pusher({
-    appId: "971587",
-    key: "8a8448593705331f8283",
-    secret: "f362ffdac8b6be9b6c29",
-    cluster: "us3",
-    encrypted: true
+    appId: config.pusher.appId,
+    key: config.pusher.key,
+    secret: config.pusher.secret,
+    cluster: config.pusher.cluster,
+    useTLS: config.pusher.useTLS,
 });
 
 pusher.trigger("my-channel", "my-event", {
